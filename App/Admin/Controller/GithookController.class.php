f7bf643671f7071814eae4067fd946c726e5c4ce
c32 <amd5@qq.com>
2018-01-21 15:58:49 +0800

    2018-1-21 15:58:47

--- App/Admin/Controller/GithookController.class.php
+++ App/Admin/Controller/GithookController.class.php
@@ -13,7 +13,7 @@ class GithookController extends CommonController {
 	{
 		echo "webhooksf</br>";
         error_reporting ( E_ALL );
-        $dir = '/home/wwwroot/web/domain/blog.19aq.com/web/';//该目录为git检出目录
+        $dir = '/home/wwwroot/wwwroot/domain/coin.19aq.com/web/';//该目录为git检出目录
         $handle = popen('cd '.$dir.' && git pull 2>&1','r');
         $read = stream_get_contents($handle);
         printf($read."</br>");
